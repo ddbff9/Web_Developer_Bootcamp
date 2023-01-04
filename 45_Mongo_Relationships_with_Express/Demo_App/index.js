2075b63dd172617f3283f154c4717e29a7ee226f
Dan Bax <Dan.D.Bax@gmail.com>
2023-01-03 19:58:09 -0600

    complete sect 45 on Mongo relationships w/ Express

--- 45_Mongo_Relationships_with_Express/Demo_App/index.js
+++ 45_Mongo_Relationships_with_Express/Demo_App/index.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
 const Product = require('./models/product');
+const Farm = require('./models/farm')
 
-mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+mongoose.connect('mongodb://127.0.0.1:27017/farmStandTake2')
   .then(() => {
     console.log('MONGO CONNECTION OPEN!!!')
   })
@@ -20,6 +21,57 @@ app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
+// FARM ROUTES
+
+app.get('/farms', async (req, res)=>{
+  const farms = await Farm.find({});
+  res.render('farms/index',{farms});
+})
+
+
+
+app.post('/farms', async(req, res) =>{
+  const farm = new Farm(req.body);
+  await farm.save();
+  res.redirect('/farms')
+})
+
+app.get('/farms/new', (req, res) =>{
+  res.render('farms/new');
+})
+
+app.get('/farms/:id', async (req,res) =>{
+  const {id} = req.params;
+  const farm = await Farm.findById(id).populate('products');
+  res.render('farms/show',{farm});
+});
+
+app.delete('/farms/:id', async (req, res)=>{
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+  
+  res.redirect('/farms');
+})
+
+app.get('/farms/:id/products/new', async (req, res)=>{
+  const {id} = req.params;
+  const farm = await Farm.findById(id);
+  res.render('products/new', {categories, farm});
+});
+
+app.post('/farms/:id/products', async (req,res)=>{
+  const {id} = req.params;
+  const farm = await Farm.findById(id);
+  const {name, price, category} = req.body;
+  const product = new Product({name, price, category});
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${farm._id}`)
+})
+
+// PRODUCT ROUTES
 
 const categories = ['fruit','vegetable', 'dairy','fungi', 'baked goods'];
 
@@ -48,7 +100,7 @@ app.post('/products',async (req,res)=>{
 
 app.get('/products/:id', async (req,res) =>{
   const {id} = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate('farm', 'name');
   res.render('products/show',{product});
 });
 
--- 45_Mongo_Relationships_with_Express/Demo_App/models/farm.js
+++ 45_Mongo_Relationships_with_Express/Demo_App/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 const { Schema } = mongoose;
 
 const farmSchema = new Schema({
@@ -11,12 +12,19 @@ const farmSchema = new Schema({
     type: String,
     require: [true, 'Email required']
   },
-  prdoucts: [
+  products: [
     {
       type: Schema.Types.ObjectId,
       ref: 'Product'
     }
   ]
+});
+
+farmSchema.post('findOneAndDelete',async function(farm) {
+  if(farm.products.length){
+    const res = await Product.deleteMany({_id: {$in: farm.products}});
+    console.log(res);
+  }
 });
 
 const Farm = mongoose.model('Farm', farmSchema);
